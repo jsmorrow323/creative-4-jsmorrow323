848a99f2bbe769b08d2e9e2c7a52f185576c17f6
EC2 Default User <ec2-user@ip-172-31-39-161.us-east-2.compute.internal>
2018-11-01 03:02:16 +0000

    finished lab4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
+var https = require('https');
+var fs = require('fs');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('weather.html', { root: 'public' });
 });
 
+/* GET city page. */
+router.get('/getcity', function (req, res, next) {
+    console.log("In getcity route");
+    fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+        if(err) throw err;
+        
+        var cities = data.toString().split("\n");
+        
+        var myRe = new RegExp("^" + req.query.q);
+        console.log("MYRE: " + myRe);
+        
+        var jsonresult = [];
+        for(var i = 0; i < cities.length; i++) {
+            var result = cities[i].search(myRe); 
+            if(result != -1) {
+                console.log(cities[i]);
+                jsonresult.push({city:cities[i]});
+            } 
+        }
+        console.log("JSON RESULTS: " + jsonresult);
+        res.status(200).json(jsonresult);
+    });
+});
+
+/* GET words page. */
+router.get("/owlRoute/:word", function (req, res, next) {
+    console.log("In owlRoute route");
+    // Get info from OWL api and return to weather.html
+    var url = `https://owlbot.info/api/v1/dictionary/${req.params.word}`;
+    console.log(url);
+    https.get(url, (response) => {
+        if (response.statusCode !== 200) {
+            res.status(response.statusCode).send();
+            return;
+        }
+        var output = [];
+        response.setEncoding('utf8');
+        response.on("data", function (data) {
+            output.push(data);
+        });
+        response.on("end", function (data) {
+            var owlJson = JSON.parse(output.join(''));
+            res.status(200).json(owlJson);
+        });
+    }).on("error", function (data) {
+        res.status(500).send();
+    });
+});
 module.exports = router;
